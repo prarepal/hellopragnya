c2f3df55dfc43d35c874a51272d99f94caa0fabc
Prarepal <prarepal@cisco.com>
2022-04-09 18:24:04 -0400

    add

---
+++ say_hello.py
@@ -0,0 +1,11 @@
+# Print text to the console
+print ("hello people")
+
+# get the text entered at hte console
+
+your_name = input("What is your name")
+
+# Print text to the console
+
+print("Hello, "+ your_name +" !")
+
